526c6e413f5a632d21170f98fe21b60b1e52291e
mwikalikyalo <winniemwikali07@gmail.com>
2022-09-20 14:27:07 +0300

    Add the string reverse assessment

---
+++ rev_string.py
@@ -0,0 +1,5 @@
+def main():
+  prompt1= input("What is your name? ").strip()
+  print (prompt1[::-1])
+if __name__ == '__main__':
+    main()
--- reversal.py
+++
@@ -1,8 +0,0 @@
-def main():
-  my_name= input()
-  if my_name== str:
-    print (my_name[::-1])  
-  else:
-    print("Invalid")
-if __name__ == '__main__':
-    main()
