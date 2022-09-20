22add8233858a6fac8d8cccf883407cc31f2e70f
mwikalikyalo <winniemwikali07@gmail.com>
2022-09-20 14:16:28 +0300

    Add the sorting.py file

---
+++ reversal.py
@@ -0,0 +1,8 @@
+def main():
+  my_name= input()
+  if my_name== str:
+    print (my_name[::-1])  
+  else:
+    print("Invalid")
+if __name__ == '__main__':
+    main()
---
+++ sorting.py
@@ -0,0 +1,7 @@
+def main():
+  unsorted_numbers=[10,1,23,50,4,9,-4]
+  unsorted_numbers.sort(reverse=True)
+  print (unsorted_numbers)
+if __name__ == '__main__':
+    main()
+  
